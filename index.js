227472ad166bdaccbbb070652e87b605e481675c
Adam Malantonio <adam.malantonio@gmail.com>
2016-03-21 17:37:12 -0400

    add basic CopyResource.update functionality

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ module.exports = CopyResource;
 
 var WSKey = require('oclc-wskey');
 var https = require('https')
-
-var CR_HOST = 'circ.sd00.worldcat.org'
-var CR_PATH = '/LHR'
-var CR_URL = 'https://' + CR_HOST + CR_PATH
+var isObject = require('is-object')
+var xmlify = require('wms-xmlify-copy-resource')
 
 var noop = function(){};
 
 function CopyResource(inst, wskey) {
   if (!(wskey instanceof WSKey) && wskey.public && wskey.secret) {
     var user = {}
-    
-    if (wskey.user && wskey.user.principalID && wskey.user.principalIDNS) {
+
+    if (wskey.user && wskey.user.principalID && wskey.user.principalIDNS)
       user = wskey.user
-    }
 
     wskey = new WSKey(wskey.public, wskey.secret, user)
   }
 
   this.wskey = wskey
-  this.inst = inst || null
+  this.inst = inst
 }
 
 CopyResource.prototype.read = function readCopyResource (id, cb) {
@@ -47,41 +44,102 @@ CopyResource.prototype.search = function searchCopyResource (s, opt, cb) {
   }
 
   var startIndex = parseInt(opt.startIndex) || 1
-    , itemsPerPage = parseInt(opt.itemsPerPage) || 10
+  var itemsPerPage = parseInt(opt.itemsPerPage) || 10
 
   if ( !s['oclc'] && !s['barcode'] ) {
     return cb(new Error('Searching requires `oclc` or `barcode` property'), null);
   } else if ( s['oclc'] && s['barcode'] ) {
     return cb(new Error('Searching can not use both `oclc` and `barcode` properties'), null);
   }
-  
+
   var index = s.hasOwnProperty('oclc') ? 'oclc' : 'barcode'
-    , val = s[index]
-    , path
-  
-  path = '?q=' + index + ':' + val
+  var val = s[index]
+  var path
+
+  path = '?q=' + index + '%3A' + val
        + '&startIndex=' + startIndex
        + '&itemsPerPage=' + itemsPerPage
 
-  if ( this.inst ) path += '&inst=' + this.inst;
+  if (this.inst) path += '&inst=' + this.inst;
 
   return this._request('GET', path, cb);
 }
 
-CopyResource.prototype._request = function (method, path, callback) {
+// requests the copy resource, applies transform function + puts
+CopyResource.prototype.update = function updateCopyResource (num, opts, transform, cb) {
+  var fn = isObject(num) ? 'search' : 'read'
+  var self = this
+  var msg
+
+  // alt signature: update(num, transform, cb)
+  if (typeof opts === 'function') {
+    cb = transform
+    transform = opts
+    opts = {}
+  }
+
+  if (!cb) cb = noop
+
+  if (fn === 'search' && num['oclc']) {
+    msg = 'At this time, CopyResource.update does not support OCLC number searching'
+    return cb(new Error(msg))
+  }
+
+  var args = [num, opts, handleTransform]
+  if (fn === 'read') args.splice(1,1)
+
+  // run read/search fn
+  return self[fn].apply(self, args)
+
+  function handleTransform (err, results) {
+    if (err) return cb(err)
+
+    // search results include meta that we can't send with the update
+    if (fn === 'search') {
+      if (results.entry.length === 1) {
+        results = results.entry[0]
+      } else {
+        msg = 'Search returned multiple entries'
+        return cb(new Error(msg))
+      }
+    }
+
+    var cid = getCopyIdFromId(results.id)
+    var updated = transform(results)
+
+    return self._request('PUT', '/' + cid, xmlify(updated), cb)
+  }
+
+  function getCopyIdFromId (id) {
+    return id.replace(/^.*\/LHR\//, '').replace(/\?.*$/, '')
+  }
+}
+
+CopyResource.prototype._request = function (method, path, data, callback) {
+  var crHost = 'circ.sd00.worldcat.org'
+  var crPath = '/LHR' + path
+  var url = 'https://' + crHost + crPath
+
   var opts = {
-    hostname: CR_HOST,
+    hostname: crHost,
     method: method,
-    path: CR_PATH + path,
+    path: crPath,
     headers: {
       'Accept': 'application/json',
-      'Authorization': this.wskey.HMACSignature(method, CR_URL + path)
+      'Authorization': this.wskey.HMACSignature(method, url)
     }
   }
 
-  var req = https.request(opts, function (res) {
+  if (typeof data === 'function') {
+    callback = data
+    data = null
+  } else {
+    opts.headers['Content-Type'] = 'application/xml'
+  }
+
+  return https.request(opts, function (res) {
     var body = ''
-    res.setEncoding('utf8')    
+    res.setEncoding('utf8')
     res.on('data', function (chunk) {
       body += chunk
     })
@@ -89,43 +147,51 @@ CopyResource.prototype._request = function (method, path, callback) {
     res.on('end', function () {
       return handleOCLCResponse(res.statusCode, body, callback)
     })
-  })
+  }).end(data)
+}
 
-  req.end()
+function handleOCLCResponse(code, data, cb) {
+  var msg, parsed, err
 
-  function handleOCLCResponse(code, data, cb) {
-    var msg, parsed, err
-    if (code === 401) {
+  switch (code) {
+    case 401:
       msg = 'This request requires HTTP authentication (Unauthorized)'
-      
-      err = new Error(msg)
-      err.code = code
-      return cb(err, null)
-    }
-
-    else if (code === 404) {
+      break
+    case 403:
+      msg = 'Access to the specified resource has been forbidden.'
+      break
+    case 404:
       msg = 'The requested resource () is not available.'
-      err = new Error(msg)
-      err.code = code
-      return cb(err, null)
-    }
-
-    else if (code === 500) {
+      break
+    case 500:
       parsed = JSON.parse(data)
       msg = parsed.detail
-      err = new Error(msg)
-      err.code = code
-      return cb(err)
-    }
-
-    else {
-      parsed = JSON.parse(data)
-      if (parsed.problem) {
-        msg = parsed.problem.problemDetail || parsed.problem.problemType
-        return cb(new Error(msg))
-      } else {
-        return cb(null, parsed)
-      }
-    }
+      break
   }
+
+  if (msg) {
+    err = new Error(msg)
+    err.code = code
+    return cb(err, null)
+  }
+
+  try { parsed = JSON.parse(data) }
+  catch (e) {
+    // successful put/post requests w/o an Accept: application/json value
+    // return the xml body, which isn't JSON parseable. we shouldn't have
+    // to worry about this, but Just In Case
+    if (code === 200) return cb(null, data)
+
+    parsed = { problem: {problemDetail: 'An unknown error occurred' }}
+  }
+
+  if (parsed.problem) {
+    msg = parsed.problem.problemDetail || parsed.problem.problemType
+    err = new Error(msg)
+    err.code = code
+    err.result = data
+    return cb(err)
+  }
+
+  return cb(null, parsed)
 }
